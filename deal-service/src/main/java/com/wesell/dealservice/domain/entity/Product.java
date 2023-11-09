58b2a91f5c0fc0d0ce83aecf898adcd08fa27bfe
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-09 17:36:04 +0900

    fix: casecade 추가

--- deal-service/src/main/java/com/wesell/dealservice/domain/entity/Product.java
+++ deal-service/src/main/java/com/wesell/dealservice/domain/entity/Product.java
@@ -1,4 +1,32 @@
 package com.wesell.dealservice.domain.entity;
 
+import com.wesell.dealservice.domain.SaleStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity @Getter @Setter
+@Table(name = "product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "p_id", nullable = false)
+    private String id;
+    private String uuid;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Category category;
+    @Column(name = "p_title", nullable = false)
+    private String title;
+    @Column(name = "p_link", nullable = false)
+    private String link;
+    @Column(name = "p_detail", nullable = false)
+    private String detail;
+    @Column(name = "p_status", nullable = false)
+    private SaleStatus status;
+
+    public Product(String id, String userId, Category category, String title) {
+
+    }
 }
