fb64f7cddddd427a8f9ba909c6740e0a8df56057
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-09 11:30:12 +0900

    fix: 사용하지 않은 import 삭제

--- deal-service/src/main/java/com/wesell/dealservice/domain/entity/Category.java
+++ deal-service/src/main/java/com/wesell/dealservice/domain/entity/Category.java
@@ -1,7 +1,6 @@
 package com.wesell.dealservice.domain.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
