267c13e95c3dad514faa337a499ed6fbbbf7de7b
hyensukim <crazymin17@naver.com>
2023-11-19 11:11:10 +0900

    feat : CustomException 구현 일괄 예외처리 구현.

--- authentication-server/src/main/java/com/wesell/authenticationserver/response/ErrorCode.java
+++ authentication-server/src/main/java/com/wesell/authenticationserver/response/ErrorCode.java
@@ -1,6 +1,7 @@
 package com.wesell.authenticationserver.response;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 /**
  * 예외 일괄 처리 : 커스텀 에러코드  - 구현중
@@ -11,21 +12,29 @@ public enum ErrorCode {
     /**
      * common
      */
-    INVALID_REQUEST(400,"C001","유효하지 않은 요청입니다."),
-    INVALID_RESPONSE(400,"C002","유효하지 않은 응답입니다."),
-    NOT_FOUND_RESOURCE(404,"C003","찾을 수 없는 리소스입니다."),
-    TEMPORARY_SERVER_ERROR(400,"C004","일시적으로 서버 내 오류가 발생했습니다.");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST,"C001","유효하지 않은 요청입니다."),
+    INVALID_RESPONSE(HttpStatus.BAD_REQUEST,"C002","유효하지 않은 응답입니다."),
+    NOT_FOUND_RESOURCE(HttpStatus.NOT_FOUND,"C003","찾을 수 없는 리소스입니다."),
+    TEMPORARY_SERVER_ERROR(HttpStatus.BAD_REQUEST,"C004","일시적으로 서버 내 오류가 발생했습니다."),
 
     /**
      *  user
      */
+    NOT_SIGNUP_USER(HttpStatus.NOT_FOUND,"U001","가입된 회원이 아닙니다."),
+    MISMATCH_PASSWORD(HttpStatus.BAD_REQUEST, "U002", "비밀번호가 일치하지 않습니다."),
+
+    /**
+     * token
+     */
+    INVALID_JWT_TOKEN(HttpStatus.BAD_REQUEST,"T001","유효 하지 않은 JWT token 입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"T001","유효 하지 않은 Refresh token 입니다.");
 
 
-    private final int status; // 상태코드(숫자)
+    private final HttpStatus status; // 상태코드(숫자)
     private final String code; // 커스텀 코드(서버 내 관리하는 에러 코드)
     private final String message; // 에러 발생 시 메시지
 
-    private ErrorCode(int status, String code, String message){
+    private ErrorCode(HttpStatus status, String code, String message){
         this.status = status;
         this.code = code;
         this.message = message;
