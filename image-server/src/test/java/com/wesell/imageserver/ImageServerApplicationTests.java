b106ae571367104149de71d52e06d3e50645315e
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-21 15:33:21 +0900

    feat: 민감정보 처리

---
+++ image-server/src/test/java/com/wesell/imageserver/ImageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesell.imageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
