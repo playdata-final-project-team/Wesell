0f6e5a8294a67b9df30e7e0a02ab8b1e7087c326
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-30 12:16:45 +0900

    fix: image server dowm

--- image-server/src/main/java/com/wesell/imageserver/domain/dto/request/FileUploadRequestDto.java
+++
@@ -1,2 +0,0 @@
-package com.wesell.imageserver.domain.dto.request;public class FileUploadRequestDto {
-}
--- image-server/src/main/java/com/wesell/imageserver/service/AmazonS3ResourceStorage.java
+++
@@ -1,2 +0,0 @@
-package com.wesell.imageserver.service;public class AmazonS3ResourceStorage {
-}
