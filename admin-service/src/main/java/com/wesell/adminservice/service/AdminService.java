35c0ad39d26b221c43e95948798ee28a9a9ec634
99cp <103494820+99cp@users.noreply.github.com>
2023-12-07 10:27:12 +0900

    feat: 각 기능 예외처리

--- admin-service/src/main/java/com/wesell/adminservice/service/AdminService.java
+++ admin-service/src/main/java/com/wesell/adminservice/service/AdminService.java
@@ -4,15 +4,16 @@ import com.wesell.adminservice.dto.request.ChangeRoleRequestDto;
 import com.wesell.adminservice.dto.response.AdminUserResponseDto;
 import com.wesell.adminservice.dto.response.PostListResponseDto;
 import com.wesell.adminservice.dto.response.UserListResponseDto;
+import com.wesell.adminservice.error.ErrorCode;
+import com.wesell.adminservice.error.InternalServerException;
 import com.wesell.adminservice.feignClient.AuthFeignClient;
 import com.wesell.adminservice.feignClient.DealFeignClient;
 import com.wesell.adminservice.feignClient.UserFeignClient;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,42 +22,59 @@ public class AdminService {
     private final UserFeignClient userFeignClient;
     private final AuthFeignClient authFeignClient;
     private final DealFeignClient dealFeignClient;
+    private static final Logger logger = LoggerFactory.getLogger(AdminService.class);
 
-    public Page<UserListResponseDto> getUserList(int page, int size){
-        return userFeignClient.getUserList(page, size);
-    }
-
-    public void changeUserRole(ChangeRoleRequestDto requestDto) {
-        ResponseEntity<String> response = authFeignClient.changeUserRole(requestDto);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            System.out.println("User role changed successfully");
-        } else {
-            System.out.println("Failed to change user role: " + response.getBody());
+    public Page<UserListResponseDto> getUserList(int page, int size) {
+        try {
+            return userFeignClient.getUserList(page, size);
+        } catch (Exception e) {
+            logger.error("유저 전체 목록 조회 오류 발생", e);
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR_GET_USER_LIST);
         }
     }
 
-    public Page<PostListResponseDto> getPostList(String uuid,
-                                                 int page,
-                                                 int size) {
-        return dealFeignClient.getPostList(uuid, page, size);
+    public void changeUserRole(ChangeRoleRequestDto requestDto) {
+        try {
+            authFeignClient.changeUserRole(requestDto);
+        } catch (Exception e) {
+            logger.error("회원 권한 변경 오류 발생", e);
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR_CHANGE_USER_ROLE);
+        }
     }
 
-    public String updateIsForced(String uuid) {
-        return authFeignClient.updateIsForced(uuid).getBody();
+    public Page<PostListResponseDto> getPostList(String uuid, int page, int size) {
+        try {
+            return dealFeignClient.getPostList(uuid, page, size);
+        } catch (Exception e) {
+            logger.error("글 목록 조회 오류 발생", e);
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR_GET_POST_LIST);
+        }
     }
 
-    public void deletePost(String uuid, Long postId) {
-        dealFeignClient.deletePost(uuid, postId);
+    public String userIsForced(String uuid) {
+        try {
+            return authFeignClient.updateIsForced(uuid).getBody();
+        } catch (Exception e) {
+            logger.error("회원 강제 탈퇴 오류 발생", e);
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR_USER_ISFORCED);
+        }
     }
 
-    public Page<AdminUserResponseDto> searchUsers(String name,
-                                                  String nickname,
-                                                  String phone,
-                                                  String uuid,
-                                                  int page,
-                                                  int size) {
+    public void postIsDeleted(String uuid, Long postId) {
+        try {
+            dealFeignClient.deletePost(uuid, postId);
+        } catch (Exception e) {
+            logger.error("글 강제 삭제 오류 발생", e);
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR_POST_ISDELETED);
+        }
+    }
 
-        return userFeignClient.searchUsers(name, nickname, phone, uuid, page, size);
+    public Page<AdminUserResponseDto> searchUsers(String name, String nickname, String phone, String uuid, int page, int size) {
+        try {
+            return userFeignClient.searchUsers(name, nickname, phone, uuid, page, size);
+        } catch (Exception e) {
+            logger.error("회원 목록 검색 오류 발생", e);
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR_SEARCH_USERS);
+        }
     }
 }
