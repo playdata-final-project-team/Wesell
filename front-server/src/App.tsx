e829be1d60a0e1ee1c14475a5f710f6bce3452b7
parkjiho <agumonn43@naver.com>
2023-12-19 11:28:04 +0900

    feat: 아이디 찾기, 비밀번호 찾기 및 비밀번호 수정 페이지 구현

--- front-server/src/App.tsx
+++ front-server/src/App.tsx
@@ -10,6 +10,11 @@ import Social from 'pages/Social/kakao';
 import UploadBoard from 'pages/board/write/Write';
 import PostDetailPage from 'pages/board/detail';
 import EditPostPage from 'pages/board/edit';
+import UuidComponent from 'pages/Userfindbyid/UuidComponent';
+import FoundEmailComponent from 'pages/Userfindbyid/FoundEmailComponent';
+import EmailfindComponent from 'pages/UserfindbyPwd/EmailfindComponent';
+import FoundSmsComponent from 'pages/UserfindbyPwd/FoundSmsComponent';
+import PasswordUpdateComponent from 'pages/UserfindbyPwd/PasswordUpdateComponent';
 
 // component: Application 컴포넌트 //
 function App() {
@@ -34,6 +39,11 @@ function App() {
         <Route path={BOARD_DETAIL()} element={<PostDetailPage />} />
         <Route path={BOARD_EDIT()} element={<EditPostPage />} />
         <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+        <Route path='testJiho2' element = {<UuidComponent/>}/>
+        <Route path='/found-email/:uuid' element = {<FoundEmailComponent/>}/>
+        <Route path='testJiho4' element = {<EmailfindComponent/>}/>
+        <Route path='testJiho5/:uuid' element = {<FoundSmsComponent/>}/>
+        <Route path='/testJiho6/:uuid' element = {<PasswordUpdateComponent/>}/>
       </Route>
     </Routes>
   );
