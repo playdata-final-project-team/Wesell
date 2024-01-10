a4a39ed123f3e0cc483626be13923b2d1dcb1b1a
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2024-01-10 09:39:09 +0900

    feat: 결제 api 의존성 추가

---
+++ pay-service/src/main/java/com/wesell/payservice/PayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wesell.payservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayServiceApplication.class, args);
+    }
+
+}
