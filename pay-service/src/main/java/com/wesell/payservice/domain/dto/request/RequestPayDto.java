363bd445a3bab9bcc5d9d54b213d0f7f31822808
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2024-01-14 02:11:28 +0900

    feat: create delivery 기능 구현

--- pay-service/src/main/java/com/wesell/payservice/domain/dto/request/RequestPayDto.java
+++ pay-service/src/main/java/com/wesell/payservice/domain/dto/request/RequestPayDto.java
@@ -11,14 +11,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RequestPayDto {
     @NotNull
+    private Long deliveryId;
+    @NotNull
     private String buyer;
     @NotBlank
     private Long productId;
     @NotBlank
     private Integer type;
-    @NotBlank
-    private Long amount;
-    @NotNull
-    private String address;
-
 }
