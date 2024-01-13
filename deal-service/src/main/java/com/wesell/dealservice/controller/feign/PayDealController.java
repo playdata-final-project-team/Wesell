c331112108e18fd15220aa37f1dc7e9ac4e24de9
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2024-01-14 02:04:04 +0900

    feat: 페인 통신으로 가격 데이터 전송

--- deal-service/src/main/java/com/wesell/dealservice/controller/feign/PayDealController.java
+++ deal-service/src/main/java/com/wesell/dealservice/controller/feign/PayDealController.java
@@ -1,2 +1,22 @@
-package com.wesell.dealservice.controller.feign;public class PayDealController {
+package com.wesell.dealservice.controller.feign;
+
+import com.wesell.dealservice.service.feign.PayDealService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class PayDealController {
+    private final PayDealService payDealService;
+    public PayDealController(PayDealService payDealService) {
+        this.payDealService = payDealService;
+    }
+
+    @GetMapping("pay/payInfo/{postId}")
+    public ResponseEntity<Long> getPrice(@RequestParam("postId") Long postId) {
+        return ResponseEntity.ok(payDealService.getPrice(postId));
+    }
 }
--- deal-service/src/main/java/com/wesell/dealservice/domain/repository/read/ViewDao.java
+++ deal-service/src/main/java/com/wesell/dealservice/domain/repository/read/ViewDao.java
@@ -1,2 +1,12 @@
-package com.wesell.dealservice.domain.repository.read;public interface ViewDao {
+package com.wesell.dealservice.domain.repository.read;
+
+import com.wesell.dealservice.domain.entity.DealPost;
+import feign.Param;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+
+public interface ViewDao extends Repository<DealPost, Long> {
+    @Query("SELECT dp.price FROM DealPost dp "
+            + "WHERE dp.id =:postId")
+    Long findPriceByPostId(@Param("postId") Long postId);
 }
