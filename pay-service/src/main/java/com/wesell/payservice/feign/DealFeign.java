025008b8cf0807e5f1c610585e798f35eea6cb4e
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2024-01-14 02:10:34 +0900

    feat: 페인 통신 엔드포인트 및 추가 설정

--- pay-service/src/main/java/com/wesell/payservice/feign/DealFeign.java
+++ pay-service/src/main/java/com/wesell/payservice/feign/DealFeign.java
@@ -1,4 +1,12 @@
 package com.wesell.payservice.feign;
 
-public class DealFeign {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "deal-service")
+public interface DealFeign {
+    @GetMapping("api/v1/pay/payInfo/{postId}")
+    Long getPayInfo(@PathVariable(value = "postId") Long postId);
+
 }
