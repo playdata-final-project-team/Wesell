729c1cc3abaa6f791bb057b899acb81049a12a24
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2024-01-14 13:38:30 +0900

    fix: 페인 통신 연결 오류 해결

--- pay-service/src/main/java/com/wesell/payservice/feign/DealFeign.java
+++ pay-service/src/main/java/com/wesell/payservice/feign/DealFeign.java
@@ -4,9 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//Request to deal-service
 @FeignClient(name = "deal-service")
 public interface DealFeign {
-    @GetMapping("api/v1/pay/payInfo/{postId}")
+    @GetMapping("api/v2/payments/{postId}/amount")
     Long getPayInfo(@PathVariable(value = "postId") Long postId);
 
 }
--- pay-service/src/main/java/com/wesell/payservice/service/pay/PayServiceImpl.java
+++ pay-service/src/main/java/com/wesell/payservice/service/pay/PayServiceImpl.java
@@ -2,7 +2,9 @@ package com.wesell.payservice.service.pay;
 
 import com.wesell.payservice.domain.dto.request.RequestPayDto;
 import com.wesell.payservice.domain.dto.response.ResponsePayDto;
+import com.wesell.payservice.domain.entity.Delivery;
 import com.wesell.payservice.domain.entity.Pay;
+import com.wesell.payservice.domain.repository.DeliveryRepository;
 import com.wesell.payservice.domain.repository.PayRepository;
 import com.wesell.payservice.feign.DealFeign;
 import jakarta.transaction.Transactional;
@@ -16,17 +18,29 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class PayServiceImpl implements PayService {
     private final PayRepository payRepository;
+    private final DeliveryRepository deliveryRepository;
     private final DealFeign dealFeign;
-    public PayServiceImpl(PayRepository payRepository, DealFeign dealFeign) {
+    public PayServiceImpl(PayRepository payRepository, DeliveryRepository deliveryRepository, DealFeign dealFeign) {
         this.payRepository = payRepository;
+        this.deliveryRepository = deliveryRepository;
         this.dealFeign = dealFeign;
     }
 
     @Override
     public Long createPay(RequestPayDto requestDto) {
-        Pay pay = Pay.createPay(requestDto, createOrderNumber(requestDto), dealFeign.getPayInfo(requestDto.getProductId()));
-        payRepository.save(pay);
-        return pay.getId();
+        Long amount = dealFeign.getPayInfo(requestDto.getProductId());
+        Pay pay = Pay.createPay(requestDto, createOrderNumber(requestDto), amount);
+
+        //todo: 결제 시도 횟수
+        //todo: 프론트에서 -1 을 받는 경우, 결제 실패 처리! -> 다시 시도? ㅜㅜ
+        if(amount == null || amount == 0){
+            Delivery delivery = deliveryRepository.findDeliveryById(pay.getDeliveryId());
+            deliveryRepository.delete(delivery);
+            return -1L;
+        } else {
+            payRepository.save(pay);
+            return pay.getId();
+        }
     }
 
     @Override
