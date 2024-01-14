a5059b11f05a9530b6b78b31421796b95c85a487
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2024-01-14 14:45:50 +0900

    fix: 페인 통신 엔드포인트 수정

--- deal-service/src/main/java/com/wesell/dealservice/controller/feign/PayDealController.java
+++ deal-service/src/main/java/com/wesell/dealservice/controller/feign/PayDealController.java
@@ -3,20 +3,20 @@ package com.wesell.dealservice.controller.feign;
 import com.wesell.dealservice.service.feign.PayDealService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v2")
 public class PayDealController {
     private final PayDealService payDealService;
     public PayDealController(PayDealService payDealService) {
         this.payDealService = payDealService;
     }
 
-    @GetMapping("pay/payInfo/{postId}")
-    public ResponseEntity<Long> getPrice(@RequestParam("postId") Long postId) {
+    @GetMapping("payments/{postId}/amount")
+    public ResponseEntity<Long> getPriceByPostId(@PathVariable Long postId) {
         return ResponseEntity.ok(payDealService.getPrice(postId));
     }
 }
