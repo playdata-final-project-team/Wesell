599e2aca036a866d63c10d822de05fa37595376d
hyensukim <crazymin17@naver.com>
2024-01-22 17:10:35 +0900

    feat: chat-service에서 채팅방 이미지 및 최근 메시지 전송일 가져오기

--- chat-service/src/main/java/com/wesell/chatservice/service/ChatRoomLoadServiceImpl.java
+++ chat-service/src/main/java/com/wesell/chatservice/service/ChatRoomLoadServiceImpl.java
@@ -1,64 +1,52 @@
 package com.wesell.chatservice.service;
 
 import com.wesell.chatservice.domain.entity.ChatRoom;
+import com.wesell.chatservice.domain.feign.ImageFeignService;
 import com.wesell.chatservice.domain.repository.ChatRoomRepository;
 import com.wesell.chatservice.domain.service.ChatRoomLoadService;
 import com.wesell.chatservice.dto.query.ChatRoomListQuery;
-import com.wesell.chatservice.dto.query.ChatRoomQuery;
 import com.wesell.chatservice.dto.response.ChatRoomListResponseDto;
 import com.wesell.chatservice.dto.response.ChatRoomResponseDto;
-import com.wesell.chatservice.exception.ChatRoomNotFoundException;
+import com.wesell.chatservice.global.util.SearchUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomLoadServiceImpl implements ChatRoomLoadService {
     private final ChatRoomRepository chatRoomRepository;
-
-    @Override
-    public ChatRoomResponseDto getChatRoomById(ChatRoomQuery query) {
-        ChatRoom room = chatRoomRepository.findById(query.getId()).orElseThrow(
-                () -> new ChatRoomNotFoundException()
-        );
-
-//        if(room.getChatMessageList().isEmpty()){
-//            return ChatRoomResponseDto.builder()
-//                    .roomId(room.getId())
-//                    .build();
-//        }
-//
-//        List<ChatMessageResponseDto> chatMessageList = room.getChatMessageList().stream().map((entity) ->
-//                ChatMessageResponseDto.builder()
-//                        .id(entity.getId())
-//                        .message(entity.getContent())
-//                        .sender(entity.getSender())
-//                        .sendDate(entity.getSendDate().toString())
-//                        .build()
-//        ).collect(Collectors.toList());
-
-        return ChatRoomResponseDto.builder()
-                .roomId(room.getId())
-                .build();
-    }
+    private final ImageFeignService imageFeignService;
+    private final SearchUtil searchUtil;
+    private Map<Long,String> urls;
 
     @Override
     public ChatRoomListResponseDto getChatRoomList(ChatRoomListQuery query) {
-        Slice<ChatRoom> chatRoomSlice = chatRoomRepository
-                .findAllByConsumer(PageRequest.of(query.getPage(), query.getSize()), query.getConsumer());
+        Slice<ChatRoom> chatRoomSlice = chatRoomRepository.findAllByConsumer(query.getConsumer(),
+                PageRequest.of(query.getPage(), query.getSize(), Sort.by("id").descending()));
+
+        initImageUrl(chatRoomSlice);
+
         return ChatRoomListResponseDto.builder()
-                .roomList(chatRoomSlice.getContent().stream().map(
-                        (entity) -> ChatRoomResponseDto.builder()
-                                .roomId(entity.getId())
-                                .seller(entity.getSeller())
-                                .build()
-                ).collect(Collectors.toList()))
-                .hasNext(chatRoomSlice.hasNext())
-                .build();
+                .roomList(chatRoomSlice.getContent().stream().map((entity) -> ChatRoomResponseDto.builder()
+                        .roomId(entity.getId())
+                        .seller(entity.getSeller())
+                        .imageUrl(urls.get(entity.getProductId()))
+                        .lastSendDate(searchUtil.searchLastSendDate(entity))
+                        .build()).collect(Collectors.toList()))
+                .hasNext(chatRoomSlice.hasNext()).build();
     }
+
+    private void initImageUrl(Slice<ChatRoom> chatRoomSlice){
+        urls = imageFeignService.getUrlByProductId(chatRoomSlice.stream()
+                .map(ChatRoom::getProductId)
+                .map(Long::parseLong)
+                .collect(Collectors.toList()));
+    }
+
+
 }
