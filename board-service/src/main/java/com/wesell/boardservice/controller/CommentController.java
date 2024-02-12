b7581be8e63af541c9ab916cd99ca493a07213f5
bhPark98 <138194791+bhPark98@users.noreply.github.com>
2024-02-12 18:51:38 +0900

    fix: 조회수 오류 및 댓글 중복 오류 관련 처리

--- board-service/src/main/java/com/wesell/boardservice/controller/CommentController.java
+++ board-service/src/main/java/com/wesell/boardservice/controller/CommentController.java
@@ -16,17 +16,17 @@ import java.util.List;
 public class CommentController {
 
     private final CommentService commentService;
-
+    // 댓글 조회
     @GetMapping("/comments/{postId}")
     public ResponseEntity<List<CommentResponseDto>> findAllCommentsByPostId(@PathVariable("postId") Long postId) {
         return ResponseEntity.ok(commentService.findCommentsByPostId(postId));
     }
-
+    // 댓글 생성
     @PostMapping("/comments")
     public ResponseEntity<CommentResponseDto> createComment(@RequestBody CommentRequestDto commentRequestDto) {
         return ResponseEntity.ok(commentService.createComment(commentRequestDto));
     }
-
+    // 댓글 삭제
     @DeleteMapping("/comments/{commentId}")
     public void deleteComment(@PathVariable("commentId") Long commentId) {
         commentService.deleteComment(commentId);
--- board-service/src/main/java/com/wesell/boardservice/error/ErrorCode.java
+++ board-service/src/main/java/com/wesell/boardservice/error/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "400", "존재하지 않는 게시물입니다."),
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "400", "댓글을 찾을 수 없습니다.");
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "400", "댓글을 찾을 수 없습니다."),
+    FEIGN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500", "서버가 다운되었습니다.");
 
     private final HttpStatus status;
     private final String code;
